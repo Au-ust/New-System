6fbb22d4d315b3abb0b54b097ab19a4aabe50687
August <2837904488@qq.com>
2025-03-26 21:42:01 +0800

    json.server和SideMenu优化

--- src/components/sandbox/SideMenu.jsx
+++ src/components/sandbox/SideMenu.jsx
@@ -1,45 +1,94 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+//控制路由
+import { useLocation, useNavigate } from "react-router-dom";
 //从antd引入Layout组件的子组件Sider
-import { Layout ,Menu,theme} from "antd";
+import { Layout ,Menu} from "antd";
 //引入antd的图标组件
 import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
-
+import './index.css';
+//引入css
+import './index.css';
+import axios from "axios";
 //解构Layout
 const { Header, Content, Sider } = Layout;
 
-
 function SideMenu() {
+  // 图标映射表（根据需要自行扩展）
+const iconMap = {
+  "/audit-manage": <UserOutlined />,
+  "/publish-manage": <UploadOutlined />,
+  "/audit-manage/audit": <VideoCameraOutlined />,
+  "default": <AppstoreOutlined />
+};
+
+  const [menuItems, setMenuItems] = useState([]);
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
+  //获取路径名
+  const pathname = useLocation().pathname;
+  //截取一级路由
+  const openKeys = [pathname.split("/").slice(0, 2).join("/")];
+
+
+  const checkPermission = (item) => { 
+    if (item.pagepermisson === 1) { 
+      return true; 
+    } 
+    if (item.children) { 
+      return item.children.some(child => checkPermission(child)); 
+    } 
+    return false;
+  }
+
+  //声明格式化 JSON 数据为 AntD 所需的 items 格式的函数
+  const formatMenu = (data) => {
+    return data
+      .filter(item =>checkPermission(item))//过滤pagepermisson
+      .map(item => ({
+        key: item.key,
+        icon: iconMap[item.key] || iconMap["default"],
+        label: item.title,
+        //递归调用，判断是否有children，再判断children是否有长度
+        children: item.children?.length > 0 ? formatMenu(item.children) : undefined
+      }));
+    };
+     //获取数据
+  useEffect(() => { 
+      //调用格式化函数，格式数据
+      axios.get('http://localhost:3000/rights?_embed=children') .then(response => setMenuItems(formatMenu(response.data)))
+        .catch(error => console.error("加载失败：", error));
+    }, []);
     
-    const [collapsed, setCollapsed] = useState(false);
-    return (
+   const handleMenuClick = (e) => {
+    navigate(e.key);  // 切换路由
+  };
+  return (
+       //collapsible可折叠的，是否可收起；collapsed当前的折叠状态
        <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
-        <Menu
+       <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
+       <div className="logo">全球新闻发布管理系统</div>
+        <div style={{flex: 1, overflow: 'auto'}}>
+          <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
+        /*defaultSelectedKeys默认选中的菜单项 key 数组，打开默认的item是对应key的值,这里是home*/
+            defaultSelectedKeys={['1']}
+        /*selectedKeys当前选中的菜单项 key 数组*/
+            selectedKeys={pathname}
+        /*defaultOpenKeys默认展开的 SubMenu 菜单项 key 数组,只截取一级路由*/
+            defaultOpenKeys={openKeys}
+          items={menuItems}
+          onClick={handleMenuClick}
         />
+        </div>
+      </div>
       </Sider>
     );
 }
--- src/views/sandbox/home/Home.jsx
+++ src/views/sandbox/home/Home.jsx
@@ -1,9 +1,43 @@
 
 import { Button } from "antd";
+import axios from "axios";
 function Home() {
+
+    const ajax = () => {
+        //取数据
+        // axios.get('http://localhost:3000/posts').then(res => {
+        //     console.log(res.data)
+        // })
+        //增数据
+        // axios.post('http://localhost:3000/posts', {
+        //     title: '标题',
+        //     author: '作者'
+        // })
+        // axios.put('http://localhost:3000/posts/1', {
+        //     title: '标题111',
+        //     author: '作者111'
+        // })
+        //更新数据
+        // axios.patch('http://localhost:3000/posts/1', {
+        //     title: '标题22222',
+        // })
+        //删除 delete
+        // axios.delete('http://localhost:3000/posts/1')
+
+        //_embed
+        // axios.get('http://localhost:3000/posts?_embed=comments').then(res => {
+        //     console.log(res.data)
+        // })
+        //_expand
+        // axios.get('http://localhost:3000/comments?_expand=post').then(res => {
+        //     console.log(res.data)
+        // })
+
+    }
+
     return (
         <div>
-           
+            <Button type="primary" onClick={ajax}>Primary Button</Button>
         </div>
     );
 }
